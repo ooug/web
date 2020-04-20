3bada833fa5afcc0dd3f8815402a39b1da68a8ad
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-04-20 17:43:04 +0530

    worked on home,footer,upcoming events,articles/blogs

--- src/app/pages/home/components/upcoming-events/upcoming-events.component.ts
+++ src/app/pages/home/components/upcoming-events/upcoming-events.component.ts
@@ -6,7 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upcoming-events.component.scss']
 })
 export class UpcomingEventsComponent implements OnInit {
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  public upcoming_events = [
+    {
+      img_link: '../../../../../assets/imgs/gray.jpg',
+      title: 'Upcoming Event 1',
+      description: 'Some data about the event. Not more than 2 lines.',
+      link: '#'
+    },
+    {
+      img_link: '../../../../../assets/imgs/gray.jpg',
+      title: 'Upcoming Event 2',
+      description: 'Some data about the event. Not more than 2 lines.',
+      link: '#'
+    }, {
+      img_link: '../../../../../assets/imgs/gray.jpg',
+      title: 'Upcoming Event 3',
+      description: 'Some data about the event. Not more than 2 lines.',
+      link: '#'
+    }
+  ]
+
+  ngOnInit(): void { }
 }
