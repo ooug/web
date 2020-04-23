045ef6da7e2992b1d21032e2960ff2fcfe4b9edc
santosh mahto <mahtos480@gmail.com>
2020-04-24 01:27:59 +0530

    sidebar issue fixed

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,49 +1,54 @@
-import { HostListener, Component, OnInit } from '@angular/core';
+import { HostListener, Component, OnInit, OnDestroy } from '@angular/core';
 import { ViewportScroller } from '@angular/common';
 import { Router, NavigationStart } from '@angular/router';
-import { ToggleSidebar } from '../sidebar/toggleSidebar.service';
+import { Subscription } from 'rxjs';
+import { HelperService } from 'src/app/services/helper/helper.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  private subs = new Subscription();
+  private isHomePage = false;
+  public isSideBarCollapse: boolean;
+  public isTransparent = true;
+
   constructor(
     private scroll: ViewportScroller,
     private router: Router,
-    public sidebarService: ToggleSidebar
+    public helperService: HelperService
   ) {}
-
-  public isNavbarCollapsed = true;
-  public headerStyles = { 'background-color': '#652e90' };
-  private isHomePage = false;
-
-  toggleSidebar() {
-    this.sidebarService.toggleSidebar();
-  }
-
-  ngOnInit(): void {
-    this.router.events.subscribe((val) => {
-      if (val instanceof NavigationStart) {
-        this.isHomePage = val.url === '/' ? true : false;
-      }
-      if (this.isHomePage) {
-        this.headerStyles['background-color'] = 'transparent';
-      } else {
-        this.headerStyles['background-color'] = '#652e90';
-      }
-    });
-  }
 
   @HostListener('window:scroll', [])
   onScroll() {
     const yOffset = this.scroll.getScrollPosition()[1];
     if (yOffset > 10 && this.isHomePage) {
-      this.headerStyles['background-color'] = '#652e90';
+      this.isTransparent = false;
     }
+
     if (yOffset <= 10 && this.isHomePage) {
-      this.headerStyles['background-color'] = 'transparent';
+      this.isTransparent = true;
     }
+  }
+
+  ngOnInit(): void {
+    this.router.events.subscribe((val) => {
+      if (val instanceof NavigationStart) {
+        this.isHomePage = val.url === '/';
+      }
+      this.isTransparent = this.isTransparent;
+    });
+
+    this.subs.add(
+      this.helperService.isSideBarCollapse$.subscribe(($) => {
+        this.isSideBarCollapse = $;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
   }
 }
--- src/app/services/helper/helper.service.ts
+++ src/app/services/helper/helper.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HelperService {
+  private isSideBarCollapseState = true;
+  private isSideBarCollapse = new BehaviorSubject<boolean>(
+    this.isSideBarCollapseState
+  );
+  public readonly isSideBarCollapse$ = this.isSideBarCollapse.asObservable();
   constructor() {}
+
+  public toggleSideBar(): void {
+    this.isSideBarCollapseState = !this.isSideBarCollapseState;
+    this.isSideBarCollapse.next(this.isSideBarCollapseState);
+  }
 }
