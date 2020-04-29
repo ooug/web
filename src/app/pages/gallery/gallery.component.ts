a99e6082ebe8f237e0f73f3b354a47d62fcf16ab
Ashis ku patel <ashispatel1998@gmail.com>
2020-04-29 20:52:31 +0530

    gallery slider part is completed

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   constructor() {}
 
   ngOnInit(): void {}
