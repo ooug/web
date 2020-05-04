e9fbeb17db342820acf33356e6d4f4d43ba69c48
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-05-04 12:04:05 +0530

    Subscribe and login form auth, dummy api in login

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { NgForm, FormGroup, FormControl } from '@angular/forms';
 import { Title, Meta } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -8,14 +9,19 @@ import { Title, Meta } from '@angular/platform-browser';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  constructor(private titleService: Title, private metaService: Meta) {}
+  constructor(
+    private titleService: Title,
+    private metaService: Meta,
+    private http: HttpClient
+  ) {}
 
   public usernameClass = ['input-div']; // classes used for username field
   public passwordClass = ['input-div']; // classes used for password field
-  public user = {
-    username: '',
-    password: '',
-  };
+
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
 
   // when focused on input fields
   focus(input: string) {
@@ -30,23 +36,41 @@ export class LoginComponent implements OnInit {
   // when focused out
   focusout(input: string) {
     if (input === 'password') {
-      if (this.user.password === '') {
+      if (this.loginForm.value.password === '') {
         this.passwordClass.pop();
       }
     }
     if (input === 'username') {
-      if (this.user.username === '') {
+      if (this.loginForm.value.email === '') {
         this.usernameClass.pop();
       }
     }
   }
 
   // login
-  login(loginForm: NgForm) {
-    if (loginForm.invalid) {
+  login() {
+    if (this.loginForm.invalid) {
       return;
     }
-    console.log(loginForm);
+    this.http.get('./assets/api/user.json').subscribe((users) => {
+      const user = JSON.parse(JSON.stringify(users)).find((u) => {
+        return u.email === this.loginForm.value.email;
+      });
+      if (user) {
+        // user found
+        if (user.password === this.loginForm.value.password) {
+          // login successfully
+          alert(user.name + ' logged in !');
+        } else {
+          // incorrect password
+          alert('Incorrect Password!');
+        }
+      } else {
+        // email mot valid
+        alert('Invalid Email!');
+      }
+      this.loginForm.reset();
+    });
   }
 
   updateMetaTags() {
