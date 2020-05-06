3ce970422257b7088bfe70d1d348220d26b8fe37
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-05-06 16:12:54 +0530

    corrected style and bug upcoming event section in home page and SEO in blog homepage

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogService } from './blog.service';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-blog',
@@ -7,7 +8,29 @@ import { BlogService } from './blog.service';
   styleUrls: ['./blog.component.scss'],
 })
 export class BlogComponent implements OnInit {
-  constructor(private blogService: BlogService) {}
+  constructor(private titleService: Title, private metaService: Meta) {}
 
-  ngOnInit(): void {}
+  updateMetaTags() {
+    this.titleService.setTitle('Blogs | Odisha Oracle Users Group');
+    this.metaService.updateTag({
+      name: 'description',
+      content: 'Blogs | Odisha Oracle Users Group',
+    });
+    this.metaService.updateTag({
+      property: 'og:url',
+      content: 'https://odishaoug.in/blog',
+    });
+    this.metaService.updateTag({
+      property: 'og:title',
+      content: 'Blogs | Odisha Oracle Users Group',
+    });
+    this.metaService.updateTag({
+      property: 'og:description',
+      content: 'Blogs | Odisha Oracle Users Group',
+    });
+  }
+
+  ngOnInit(): void {
+    this.updateMetaTags();
+  }
 }
