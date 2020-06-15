18b929ea81cc87dc7e307074a297c6b793af99e4
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2020-06-15 11:31:11 +0530

    contact form connected with backend

--- src/app/pages/about/components/contact/contact.component.ts
+++ src/app/pages/about/components/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-contact',
@@ -6,7 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent implements OnInit {
-  constructor() {}
+  constructor(private http: HttpClient) {}
+
+  public contactForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    subject: new FormControl(''),
+    message: new FormControl(''),
+  });
+
+  public infoText = '';
+
+  public contact(): void {
+    if (this.contactForm.invalid) {
+      return;
+    }
+    this.infoText = 'Submitting...';
+    this.http
+      .post(
+        (environment.API as string) + '/app/contact-us',
+        this.contactForm.value
+      )
+      .subscribe((data: any) => {
+        if (data.data === 'FORM_SUBMITTED') {
+          this.infoText =
+            'Submitted! Someone from our team will contact you soon.';
+          this.contactForm.reset();
+        } else {
+          this.infoText = 'Something went wrong! Please try again.';
+        }
+      });
+  }
 
   ngOnInit(): void {}
 }
