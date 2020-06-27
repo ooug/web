2dacc8abbfda2237f496b73f4c70a03d9e240f70
sandeep kumar saw <sandeepkr1084@gmail.com>
2020-06-27 20:37:47 +0530

    activity page

--- src/app/pages/activities/activities.module.ts
+++ src/app/pages/activities/activities.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
-
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { ActivitiesComponent } from './activities.component';
 import { FormsModule } from '@angular/forms';
 import { ActivitiesService } from './activities.service';
 import { HttpClientModule } from '@angular/common/http';
+import { ScrollingModule } from '@angular/cdk/Scrolling';
 
 const routes: Routes = [
   {
@@ -24,6 +23,7 @@ const routes: Routes = [
     FormsModule,
     RouterModule.forChild(routes),
     HttpClientModule,
+    ScrollingModule,
   ],
   providers: [ActivitiesService],
 })
