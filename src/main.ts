c726d7b284c05b41228b09b563620ed626fd26bc
SANTOSH MAHTO <mahtos480@gmail.com>
2021-04-25 14:40:23 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if ((environment.production || environment, staging)) {
+if ((environment.production || environment.staging)) {
   enableProdMode();
 }
 
